963d121fa38f66de221b7540143ce832662a6be5
hieudoanm <hieumdoan@gmail.com>
2023-03-04 19:51:15 +0700

    Add Unit Test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
+  basePath: isProd ? '/2700chess' : undefined,
+  assetPrefix: isProd ? '/2700chess/' : undefined,
   images: { domains: ['images.chesscomfiles.com'] },
 };
 
